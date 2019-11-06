528b774e4a6a6adc95aa3a9c393c0ba34117523b
wjfatuan <32616077+wjfatuan@users.noreply.github.com>
2019-11-06 13:15:51 -0500

    Static files

--- demoapp/settings.py
+++ demoapp/settings.py
@@ -120,7 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # REST Framework
 REST_FRAMEWORK = {
